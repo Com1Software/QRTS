57e5f8ea950147205664fd277760c27d1a826f01
Dave <dave@com1software.com>
2024-09-15 13:07:10 -0400

    update

--- Go/Pi Test Robot/main.go
+++ Go/Pi Test Robot/main.go
@@ -20,7 +20,6 @@ func main() {
 	}
 	pca0.SetChannel(0, 0, 130)
 	leg := 1
-	servo0 := pca0.ServoNew(0, nil)
 	servo1 := pca0.ServoNew(1, nil)
 	servo2 := pca0.ServoNew(2, nil)
 	servo3 := pca0.ServoNew(3, nil)
@@ -32,35 +31,36 @@ func main() {
 	servo9 := pca0.ServoNew(9, nil)
 	servo10 := pca0.ServoNew(10, nil)
 	servo11 := pca0.ServoNew(11, nil)
+	servo12 := pca0.ServoNew(12, nil)
 
 	// Angle in degrees. Must be in the range `0` to `Range`
 	for i := 0; i < 130; i++ {
 		if leg == 1 || leg == 0 {
-			servo0.Angle(i)
 			servo1.Angle(i)
 			servo2.Angle(i)
+			servo3.Angle(i)
 		}
 		if leg == 2 || leg == 0 {
-			servo3.Angle(i)
 			servo4.Angle(i)
 			servo5.Angle(i)
+			servo6.Angle(i)
 		}
 		if leg == 3 || leg == 0 {
-			servo6.Angle(i)
 			servo7.Angle(i)
 			servo8.Angle(i)
+			servo9.Angle(i)
 		}
 		if leg == 4 || leg == 0 {
-			servo9.Angle(i)
 			servo10.Angle(i)
 			servo11.Angle(i)
+			servo12.Angle(i)
 		}
 
 		time.Sleep(10 * time.Millisecond)
 	}
 
 	// Fraction as pulse width expressed between 0.0 `MinPulse` and 1.0 `MaxPulse`
-	servo0.Fraction(0.5)
+
 	servo1.Fraction(0.5)
 	servo2.Fraction(0.5)
 	servo3.Fraction(0.5)
@@ -72,5 +72,5 @@ func main() {
 	servo9.Fraction(0.5)
 	servo10.Fraction(0.5)
 	servo11.Fraction(0.5)
-
+	servo12.Fraction(0.5)
 }
