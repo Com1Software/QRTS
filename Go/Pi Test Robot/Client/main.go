6f90a156a8205da7c097d02d864421e0e7874092
Lenovo 15 <information@com1software.com>
2024-09-21 14:13:14 -0400

    update

--- Go/Pi Test Robot/Client/main.go
+++ Go/Pi Test Robot/Client/main.go
@@ -3,12 +3,46 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
+
+func FindHost() string {
+	url := ""
+	max := 130
+	timeout := 1 * time.Second
+	for x := 100; x < max; x++ {
+		fmt.Printf("Checking %s\n", url)
+		url = "192.168.1." + strconv.Itoa(x) + ":8080"
+		_, err := net.DialTimeout("tcp", url, timeout)
+		if err != nil {
+			fmt.Printf(" %s - Host not Found\n ", url)
+		} else {
+			x = max
+			fmt.Printf("Host Found at %s\n", url)
+			url = "http://" + url
+			err = os.WriteFile("ip.cfg", []byte(url), 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	return url
+}
+
+//------------------------------------------------- Main
 
 func main() {
 
-	resp, err := http.Get("http://localhost:8080")
+	xip := FindHost()
+	port := "8080"
+
+	fmt.Printf("Outbound IP  : %s Port : %s\n", xip, port)
+	resp, err := http.Get(xip)
 	if err != nil {
 		panic(err)
 	}
