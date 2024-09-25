203066e424f761eb3920d2b8187c701563c38e0d
Dave <dave@com1software.com>
2024-09-25 16:06:13 -0400

    added prg

---
+++ Go/Pi Test Robot/utils/utils.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"runtime"
+)
+
+// -------------------------------------------------- Functions
+// Openbrowser : Opens default web browser to specified url
+func Openbrowser(url string) error {
+	var cmd string
+	var args []string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start msedge"}
+	case "linux":
+		cmd = "chromium-browser"
+		args = []string{""}
+
+	case "darwin":
+		cmd = "open"
+	default:
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
